b9f07057ab54276eea7af29f18402d7601c0c3f3
Konstantin <balashov-92@mail.ru>
2020-10-08 04:00:26 +0300

    add Task model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Task(models.Model):
+    STATUSES = [
+        ('new', 'new'),
+        ('planned', 'planned'),
+        ('in work', 'in work'),
+        ('finished', 'finished'),
+    ]
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name="task")
+    created = models.DateTimeField("Дата добавления", auto_now_add=True,
+                                   db_index=True)
+    status = models.CharField(max_length=10, choices=STATUSES, default='new')
+    finished = models.DateField("Дата завершения")
+
+    def __str__(self):
+        return self.title
+
+
