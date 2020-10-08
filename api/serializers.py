d1181b902494644d9e27ba9e1eb3a30a265eb1d4
Konstantin <balashov-92@mail.ru>
2020-10-08 04:00:45 +0300

    add Task Serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,10 +1,28 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from .models import Task
+User = get_user_model()
 
 
 class TaskSerializer(serializers.ModelSerializer):
-    author = serializers.ReadOnlyField(source='author.username')
+    # author = serializers.ReadOnlyField(source='author.username')
+    finished = serializers.DateField(format="%d-%m-%Y ", input_formats=['%d-%m-%Y',])
 
     class Meta:
         fields = ('id', 'title', 'description', 'created', 'status', 'finished')
         model = Task
+
+
+class UsersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username',)
+        write_only_fields = ('password',)
+
+    def validate_username(self, data):
+        username = self.context.get('request').data.get('username')
+        if User.objects.filter(username=username):
+            raise serializers.ValidationError(
+                f"User with this username already exist.")
+        return data
+
