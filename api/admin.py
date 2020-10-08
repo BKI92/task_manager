afac66f3bb44963b8c9a17a381a16add2bf66626
Konstantin <balashov-92@mail.ru>
2020-10-08 20:02:15 +0300

    Add TaskAdmin and HistoryAdmin (Admin can see how task changed)

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Task, History
 
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'author', 'title', 'description', 'created', 'status',)
+    search_fields = ("title", "author")
+    list_filter = ("created",)
+    empty_value_display = '-пусто-'
+
+
+class HistoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'task', 'status', 'created')
+    search_fields = ('id', 'task', 'status')
+    list_filter = ('created',)
+
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(History, HistoryAdmin)
