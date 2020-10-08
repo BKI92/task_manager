3e9561e750ec4af3dabdb91a76077c6741032283
Konstantin <balashov-92@mail.ru>
2020-10-08 04:00:56 +0300

    update urls

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from api.views import TaskViewSet
+from api.views import TaskViewSet, UserViewSet
 
 router = DefaultRouter()
 router.register('tasks', TaskViewSet, basename='tasks')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth/', UserViewSet.as_view()),
+
 ]
 
